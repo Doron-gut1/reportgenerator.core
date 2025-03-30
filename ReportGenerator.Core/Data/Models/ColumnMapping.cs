c76e9f0c724c619746fb8c4146e0989af5a59b43
Doron-gut1 <hodayyae@gmail.com>
2025-03-30 15:09:16 +0300

    הוספת מודל למיפוי כותרות

---
+++ ReportGenerator.Core/Data/Models/ColumnMapping.cs
@@ -0,0 +1,33 @@
+﻿namespace ReportGenerator.Core.Data.Models
+{
+    /// <summary>
+    /// מודל המייצג מיפוי בין שמות עמודות באנגלית לעברית
+    /// </summary>
+    public class ColumnMapping
+    {
+        /// <summary>
+        /// שם הטבלה הלוגי
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// שם העמודה באנגלית
+        /// </summary>
+        public string ColumnName { get; set; }
+
+        /// <summary>
+        /// הכותרת בעברית (גנרי)
+        /// </summary>
+        public string HebrewAlias { get; set; }
+
+        /// <summary>
+        /// שם פרוצדורה ספציפית (אופציונלי)
+        /// </summary>
+        public string SpecificProcName { get; set; }
+
+        /// <summary>
+        /// כותרת ספציפית לפרוצדורה (אופציונלי)
+        /// </summary>
+        public string SpecificAlias { get; set; }
+    }
+}
