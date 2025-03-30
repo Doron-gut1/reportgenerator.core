e6ffbd2300b6fd9938293e88d9a1aae947665780
Doron-gut1 <hodayyae@gmail.com>
2025-03-30 15:09:27 +0300

    הוספת מודל לתצורת דוח

---
+++ ReportGenerator.Core/Data/Models/ReportConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace ReportGenerator.Core.Data.Models
+{
+    /// <summary>
+    /// מודל המייצג הגדרת דוח במערכת
+    /// </summary>
+    public class ReportConfig
+    {
+        /// <summary>
+        /// מזהה הדוח
+        /// </summary>
+        public int ReportID { get; set; }
+
+        /// <summary>
+        /// שם הדוח (גם שם התבנית)
+        /// </summary>
+        public string ReportName { get; set; }
+
+        /// <summary>
+        /// שמות הפרוצדורות לשליפת נתונים (מופרדות ב-;)
+        /// </summary>
+        public string StoredProcName { get; set; }
+
+        /// <summary>
+        /// כותרת הדוח
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// תיאור הדוח (אופציונלי)
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
